19f5247f54d1cc313313591ca9d4f537cef93e2e
Petros Gasteratos <ptrgast@users.noreply.github.com>
2018-02-12 23:37:29 +0200

    Add option "dpi". Parse arguments with minimist

--- src/inkscape.js
+++ src/inkscape.js
@@ -2,6 +2,8 @@ var shell = require("shelljs");
 
 var Inkscape = function () {
 
+    this._dpi = null;
+
     this._init = function () {
         var result = shell.exec("inkscape --version");
         if (result.code!=0) {
@@ -9,8 +11,20 @@ var Inkscape = function () {
         }
     }
 
+    this.setDPI = function(dpi) {
+        this._dpi = dpi;
+    }
+
     this.export = function(input, output) {
         console.log("Exporting "+input+" to "+output);
+        
+        // Create command
+        var command = "inkscape -f "+input+" -e "+output;
+        if (this._dpi!=null) {
+            command += " -d "+this._dpi;
+        }
+
+        shell.exec(command, {silent:true});
     }
 
     this._init();
--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
 #! /usr/bin/env node
 
 var glob = require("glob");
+var minimist = require("minimist");
 var inkscape = require("./inkscape");
 var helpers = require("./helpers");
 
-glob(process.argv[process.argv.length-1], function(er, files) {
+// parse arguments with minimist
+var argv = minimist(process.argv);
+var input = argv._.length>=3 ? argv._[2] : null;
+var dpi = argv.dpi ? argv.dpi : null;
+
+if (input==null) {
+    throw new Error("No input file(s) specified!");
+}
+
+glob(input, function(er, files) {
+    if (dpi!=null) {
+        inkscape.setDPI(dpi);
+    }
     for (var i=0; i<files.length; i++) {
         var inputFile = files[i];
         var outputFile = helpers.removeFileExtension(inputFile)+".png";
